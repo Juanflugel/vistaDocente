b0cf1c21052ae3aaeecc094a8d7b1dc3d880a3eb
Juanflugel <juanjosegarcian1@gmail.com>
2015-04-07 13:11:23 +0200

    bug tab no se mantienen despues de refresh resuelto.

--- app/js/Directives/dir.js
+++ app/js/Directives/dir.js
@@ -22,10 +22,10 @@ angular.module('Dirapp',[])
 		templateUrl:'html/navtab.html',
 		controller:['$scope', function ($scope){
 			$scope.navTab =[
-			{Nombre:'Contenido',classe:'active',icon:'fa fa-list-alt fa-lg',state:'Docente.Contenido()'},
-			{Nombre:'Foro ',classe:'',icon:'fa fa-comments-o fa-lg fa-fw',state:'Docente.Foro()'},
-			{Nombre:'Estudiantes',classe:'',icon:'fa fa-users fa-lg fa-fw',state:'Docente.Estudiantes()'},
-			{Nombre:'Trabajos',classe:'',icon:'fa fa-tasks fa-lg fa-fw',state:'Docente.Trabajos()'}
+			{Nombre:'Contenido', icon:'fa fa-list-alt fa-lg',state:'Docente.Contenido()'},
+			{Nombre:'Foro ', icon:'fa fa-comments-o fa-lg fa-fw',state:'Docente.Foro()'},
+			{Nombre:'Estudiantes' ,icon:'fa fa-users fa-lg fa-fw',state:'Docente.Estudiantes()'},
+			{Nombre:'Trabajos', icon:'fa fa-tasks fa-lg fa-fw',state:'Docente.Trabajos()'}
 			];
 		}]
 
